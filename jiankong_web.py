0affd03dc6b26f3544537305b346774b329f3de6
hts <hts_0000@163.com>
2019-08-05 14:01:46 +0800

    python监控web脚本王城

--- jiankong_web.py
+++ jiankong_web.py
@@ -15,26 +15,35 @@ import subprocess
 #     os.system('. /etc/init.d/functions;action error /bin/false')
 #     # print('\033[31;1merror\033[0m')
 #####################################################################
-# url = '127.0.0.1'
-# port = '80'
-#函数功能:
-#检测指定网站指定端口是否存活,存活输出确认,不存活输出失败
-# def check_web() :
-#     if os.path.exists('/etc/init.d/functions') == False :
-#         print('/etc/init.d/functions文件不存在')
-#         exit()
-#     if os.system('nc -zw 5 %s %s' %(url, port)) == 0 :
-#         os.system('. /etc/init.d/functions;action "%s %s" /bin/true' %(url, port))
-#     else :
-#         os.system('. /etc/init.d/functions;action "%s %s" /bin/false' %(url, port))
-#
-# while True :
-#     check_web()
-#     time.sleep(60)
-# # file_state = os.path.exists('/etc/init.d/functions1')
-# # print(file_state)
-# # if file_state == False:
-# #     print('ok')
+url = '127.0.0.1'
+port = '80'
+# 函数功能:
+# 检测指定网站指定端口是否存活,存活输出确认,不存活输出失败
+def check_web() :
+    if os.path.exists('/etc/init.d/functions') == False :
+        print('/etc/init.d/functions文件不存在')
+        exit()
+    http_state = subprocess.run('nc -zw 5 %s %s' %(url, port) , shell=True).returncode
+    if http_state == 0:
+
+    #http_state = os.system('nc -zw 5 %s %s &> /dev/null' %(url, port))
+    #if http_state == 0:
+
+    #http_state = int(subprocess.run("echo -e '\n' | telnet %s %s | grep 'Connected' | wc -l" %(url, port), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout)
+    #if http_state == 1:
+
+    #http_state = int(subprocess.run('nmap %s -p %s | grep open | wc -l' %(url, port), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout)
+    #if http_state == 1:
+
+    #http_state = int(subprocess.run('curl -I %s 2> /dev/null | egrep "200|301|302" | wc -l' %(url), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout)
+    #if http_state == 1 :
+
+        os.system('. /etc/init.d/functions;action "%s %s" /bin/true' %(url, port))
+    else :
+        os.system('. /etc/init.d/functions;action "%s %s" /bin/false' %(url, port))
+
+if __name__ == '__main__':
+    while True :
+        check_web()
+        time.sleep(60)
 #####################################################################
-http_state = subprocess.run('nc -zw 5 www.baidu.com 80', shell=True)
-print(http_state.returncode)
